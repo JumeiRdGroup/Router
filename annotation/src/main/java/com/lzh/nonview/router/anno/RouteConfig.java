1dd6371905a69fef150047dc2173d2fa78dca79b
haoge <470368500@qq.com>
2016-12-21 20:15:30 +0800

    add javadoc with RouteConfig

--- annotation/src/main/java/com/lzh/nonview/router/anno/RouteConfig.java
+++ annotation/src/main/java/com/lzh/nonview/router/anno/RouteConfig.java
@@ -5,9 +5,21 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * This annotation defined some basic configurations
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.TYPE})
 public @interface RouteConfig {
+    /**
+     * Define a basic schema value.when you set route rules in {@link RouterRule} without a special schema.this should be used instead
+     * @return a basic schema value
+     */
     String schema() default "";
+
+    /**
+     * Defined a basic package value.when you haven't set a package value in {@link RouterRule},this word should be used instead
+     * @return a basic package value.
+     */
     String pack() default "";
 }
