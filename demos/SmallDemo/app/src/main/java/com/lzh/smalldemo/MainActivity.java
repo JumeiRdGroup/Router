f8977b7b71af8a410f6b5debc1e1359655aab7d8
haoge <470368500@qq.com>
2017-08-13 18:37:56 +0800

    initial Small demo

---
+++ demos/SmallDemo/app/src/main/java/com/lzh/smalldemo/MainActivity.java
@@ -0,0 +1,12 @@
+package com.lzh.smalldemo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
