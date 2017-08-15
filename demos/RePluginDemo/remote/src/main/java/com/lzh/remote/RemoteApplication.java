cbca7aa4eee779c870126cef3075d9b987a15c7a
haoge <470368500@qq.com>
2017-08-15 14:49:34 +0800

    fix bug

--- demos/RePluginDemo/remote/src/main/java/com/lzh/remote/RemoteApplication.java
+++ demos/RePluginDemo/remote/src/main/java/com/lzh/remote/RemoteApplication.java
@@ -2,7 +2,10 @@ package com.lzh.remote;
 
 import android.app.Application;
 
+import com.lzh.nonview.router.RouterConfiguration;
 import com.lzh.nonview.router.anno.RouteConfig;
+import com.lzh.router.RouterRuleCreator;
+import com.lzh.stub.router.RouterLoader;
 
 @RouteConfig(baseUrl = "remote://")
 public class RemoteApplication extends Application {
@@ -10,5 +13,7 @@ public class RemoteApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        RouterLoader.get().init(this);
+        RouterConfiguration.get().addRouteCreator(new RouterRuleCreator());
     }
 }
